c93525e029757850533ec461fcc2ab7e17dba2f0
federicocao <federico.cao94@gmail.com>
2020-12-07 23:01:50 +0100

    first commit

---
+++ src/test/java/sfg/spring5jms/Spring5JmsApplicationTests.java
@@ -0,0 +1,13 @@
+package sfg.spring5jms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
